9e9cda9e3c73cb0171e9c663cb7fbd0940d28a86
Connal de Souza <connaldesouza96@gmail.com>
2019-03-24 23:27:49 -0400

    Refactoring into more classes

---
+++ Step_Time_Analysis.py
@@ -0,0 +1,39 @@
+from Gait_Analysis_Utils import *
+
+def getStepTimes(data,zenoData):
+    
+    '''
+    
+    Calculates times of steps based on maximum distance between heels.
+    Method based on "Vision-Based Gait Analysis for Senior Care"
+    
+    '''
+    
+    time = getColumn("Time Stamp", data)
+
+    
+    filter = [1/5,1/5,1/5,1/5,1/5]
+    rightHeel = getFilteredPosition("Right Heel", filter, data)   
+    leftHeel = getFilteredPosition("Left Heel", filter, data)   
+    
+    diff = np.subtract(rightHeel, leftHeel)
+    distance = np.linalg.norm(diff, axis=1)
+    
+    # plt.figure()
+    # plt.plot(time, distance)
+    # plt.show()
+    
+    maxTimes = pruneExtrema(time[sps.argrelextrema(distance, np.greater)[0]])
+    zTimes = getColumn("First Contact (sec.)", zenoData)*1000
+    
+    firstStepTime = maxTimes[0]
+    relativeStepTimes = np.subtract(maxTimes, firstStepTime)
+    zTimes -= zTimes[0]
+    
+    relativeStepTimes, zTimes, diff = alignTimes(relativeStepTimes, zTimes)
+    
+    error = np.abs(np.subtract(relativeStepTimes, zTimes))
+    
+    absoluteStepTimes = np.add(relativeStepTimes, firstStepTime)
+    
+    return relativeStepTimes, absoluteStepTimes, error
