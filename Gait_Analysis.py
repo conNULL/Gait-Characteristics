Fixed bugs and step time calculation

Gait_Analysis.py
@@ -8,11 +8,10 @@ from Step_Time_Analysis import *
 from Ground_Plane_Detection import *
 from Gait_Analysis_Method_State import *
 
-
     
 if __name__== "__main__":
     
-    FILENAME = "S2T11_params.csv"
+    FILENAME = "S2T31_params.csv"
     
     zenoFileName = re.sub("[12]_params", "_Zeno", FILENAME)
     
@@ -39,23 +38,27 @@ if __name__== "__main__":
     
 ##Step Positions
     leftHeelPositions, rightHeelPositions, leftToePositions,rightToePositions= methodState.stepPositionFunction(methodState)
-    heelSequence = getStepSequence(rightHeelPositions, leftHeelPositions)
-    toeSequence = getStepSequence(rightToePositions, leftToePositions)
+    heelSequence = getStepSequence(rightHeelPositions, leftHeelPositions, methodState)
+    toeSequence = getStepSequence(rightToePositions, leftToePositions, methodState)
+    
+    if np.array_equal(heelSequence[0], leftHeelPositions[0]):
+        firstFoot = "left"
+    else:
+        firstFoot = "right"
    
 ##Step Length and Stride Length
     strideLengths,StepLengths, strideLengthError,stepLengthError = methodState.stepAndStrideFunction(methodState)   
     printError(strideLengthError, "Stride Length")
     printError(stepLengthError, "Step Length")
-       
-    for i in range(len(leftHeelPositions)):
-        
-        print(np.array([np.matmul(methodState.Rwc, leftHeelPositions[i]),np.matmul(methodState.Rwc, rightHeelPositions[i]),np.matmul(methodState.Rwc, leftToePositions[i]),np.matmul(methodState.Rwc, rightToePositions[i])]))
     
 ##Dimensions
     
+    zFootLength = getColumn("Foot Length (cm.)", zenoData)
     
-     
+    footLength = np.linalg.norm(np.subtract(toeSequence, heelSequence), axis=1)
+    error = np.subtract(footLength, zFootLength)
     
-    
+    printError(error, "Foot Length")
+        
     
     

Gait_Analysis_Utils.py
@@ -24,22 +24,52 @@ def getFilteredPosition(joint, kernel, data):
     
     return np.array([x,y,z]).T
 
-def pruneExtrema(times):
+def pruneExtrema(maxTimeIndexes, time, distance):
     
         # ALPHA = 0.1
         # 
         # r = times[-1]-time[1]
         # theta = r*ALPHA
-        theta = 500
+        # theta = 500
+        # 
+        # newExtrema = [times[0]]
+        # 
+        # for i in range(1,len(times)):
+        #     if times[i] - newExtrema[-1] > theta:
+        #         newExtrema.append(times[i])
+        #         
         
-        newExtrema = [times[0]]
+        minTimeIndexes = []
+            
+        ALPHA = 0.1
         
-        for i in range(1,len(times)):
-            if times[i] - newExtrema[-1] > theta:
-                newExtrema.append(times[i])
+        for i in range(1, len(maxTimeIndexes)):
+            
+            minTimeIndexes.append(maxTimeIndexes[i-1] + np.argmin(distance[maxTimeIndexes[i-1]:maxTimeIndexes[i]]))
+            
+        minTimeIndexes.append(maxTimeIndexes[-1] + np.argmin(distance[maxTimeIndexes[-1]:]))
+        
+        maxDistances = distance[maxTimeIndexes]
+        minDistances = distance[minTimeIndexes]
+        
+        maxDistance = maxDistances[np.argsort(maxDistances)[-2]]
+        minDistance = minDistances[np.argsort(minDistances)[2]]
+        
+        threshold = ALPHA*(maxDistance-minDistance)
+        
+        indexesToKeep = []
+        
+        for i in range(len(maxTimeIndexes)):
+            
+            if maxDistances[i] - minDistances[i] > threshold:
+                indexesToKeep.append(i)
                 
+        return time[maxTimeIndexes[indexesToKeep]]
+        
+        
+        
+        
         return newExtrema
-   
 def alignTimes(arr1, arr2):
     
     if len(arr1) < len(arr2):
@@ -49,14 +79,14 @@ def alignTimes(arr1, arr2):
         swapped = False
         
     diff = len(arr1)-len(arr2)
-    offset = min(range(diff+1), key=lambda i: np.linalg.norm(np.subtract(arr1[i:i+len(arr2)], arr2)))
+    offset = min(range(diff+1), key=lambda i: np.linalg.norm(np.power(np.abs(np.subtract(arr1[i:i+len(arr2)], arr2)),[1/2])))
     
     arr1 = arr1[offset: offset+len(arr2)]
     
     if swapped:
-        return arr2, arr1, diff
+        return arr2, arr1, diff, offset
         
-    return arr1, arr2, diff
+    return arr1, arr2, diff, offset
     
 def projectPointToPlane(point, plane):
     

Step_Position_Analysis.py
@@ -30,14 +30,14 @@ def getStepPositions(methodState):
     return leftHeelPositions,rightHeelPositions,leftToePositions,rightToePositions
     
     
-def getStepSequence(leftPositions, rightPositions):
+def getStepSequence(leftPositions, rightPositions, methodState):
     
     secondStep, firstStep = getLaterPoint(rightPositions[0], leftPositions[0], methodState.walkingDirection)
     
     allPositions = []
     if np.array_equal(secondStep,rightPositions[0]):
         
-        for i in range(0,len(leftPositions),2):
+        for i in range(0,len(leftPositions)-1,2):
             allPositions.append(rightPositions[i])
             allPositions.append(leftPositions[i+1])
             
@@ -46,7 +46,7 @@ def getStepSequence(leftPositions, rightPositions):
             
     else:
         
-        for i in range(0,len(rightPositions),2):
+        for i in range(0,len(rightPositions)-1,2):
             allPositions.append(leftPositions[i])
             allPositions.append(rightPositions[i+1])
             
@@ -71,7 +71,7 @@ def getStrideAndStepLength(rightPositions,leftPositions, methodState):
     zStepLength = getColumn("Absolute Step Length (cm.)",  methodState.zenoData)
     
     
-    allPositions = getStepSequence(rightPositions,leftPositions)
+    allPositions = getStepSequence(rightPositions,leftPositions, methodState)
     
     strideLengths = []
     strideLengthErrors = []

Step_Time_Analysis.py
@@ -12,7 +12,7 @@ def getStepTimes(methodState):
     time = getColumn("Time Stamp", methodState.data)
 
     
-    filter = getMeanKernel(5)
+    filter = getMeanKernel(3)
     rightHeel = getFilteredPosition("Right Heel", filter, methodState.data)   
     leftHeel = getFilteredPosition("Left Heel", filter, methodState.data)   
     
@@ -23,23 +23,25 @@ def getStepTimes(methodState):
     # plt.plot(time, distance)
     # plt.show()
     
-    maxTimes = pruneExtrema(time[sps.argrelextrema(distance, np.greater)[0]])
+    maxTimes = pruneExtrema(sps.argrelextrema(distance, np.greater)[0], time, distance)
     zTimes = getColumn("First Contact (sec.)", methodState.zenoData)*1000
     
-    firstStepTime = maxTimes[0]
-    relativeStepTimes = np.subtract(maxTimes, firstStepTime)
+    relativeStepTimes = np.subtract(maxTimes, maxTimes[0])
     zTimes -= zTimes[0]
     
-    relativeStepTimes, zTimes, diff = alignTimes(relativeStepTimes, zTimes)
+    diffRelativeStepTimes, diffZTimes, diff,offset = alignTimes(np.diff(relativeStepTimes), np.diff(zTimes))
+    
+    relativeStepTimes = np.concatenate([[0], np.cumsum(diffRelativeStepTimes)])    
+    zTimes = np.concatenate([[0], np.cumsum(diffZTimes)])
     
     error = np.abs(np.subtract(relativeStepTimes, zTimes))
     
-    absoluteStepTimes = np.add(relativeStepTimes, firstStepTime)
+    absoluteStepTimes = np.add(relativeStepTimes, maxTimes[offset])
     
     return relativeStepTimes, absoluteStepTimes, error
     
     
-def getHeelToeTimes(methodState):
+def getHeelStrikeTimes(methodState):
     
     '''
     Calculates times of Heel Strike and Toe Off for each step.